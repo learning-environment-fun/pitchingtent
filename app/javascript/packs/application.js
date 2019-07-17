ca5199d31e75b1c6e0f8e40283c680fddac7e6c9
Alexander Rupp-Coppi <amr2311@columbia.edu>
2019-07-17 23:18:37 +0200

    clicking header arrow scrolls page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,16 @@ import "bootstrap";
 import {
   searchBarInit
 } from "./searchbar.js";
+import {
+  smoothScroll
+} from "./scroll_to_element";
 
 searchBarInit();
+
+// header arrow auto scrolling (it's not perfect)
+const downArrow = document.getElementById('header-down');
+if (downArrow !== null) {
+  downArrow.addEventListener('click', (_event) => {
+    smoothScroll('content-wrapper');
+  });
+}
