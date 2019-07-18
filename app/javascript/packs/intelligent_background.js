31e7abae09b781d4e166804545c402e3206e658c
Alexander Rupp-Coppi <amr2311@columbia.edu>
2019-07-18 13:44:15 +0200

    intelligent background refactoring

--- app/javascript/packs/intelligent_background.js
+++ app/javascript/packs/intelligent_background.js
@@ -1,20 +1,24 @@
 const intelligentBackgroundInit = () => {
-  document.querySelectorAll('.background-cover-blurred').forEach((background) => {
+  const backgrounds = document.querySelectorAll('.background-intelligent-cover').forEach((background) => {
 
     const url = background.getAttribute('data-background-image');
     const before = document.createElement('div');
     before.className = 'before';
     before.style.backgroundImage = `url('${url}')`;
+
+    // psuedo-elements can't be accessed through js; 
+    // so need to construct our own div and prepend it
+    background.appendChild(before);
+
+    // accessing zIndex needs to happen after parent is assigned
     if (background.id != 'page-container') {
       // page-container has a custom override
       before.style.zIndex = before.parentElement.zIndex - 1;
     }
 
-    // psuedo-elements can't be accessed through js; 
-    // so need to construct our own div and prepend it
-    background.appendChild(before);
     console.log(url);
   });
+  console.log(`${backgrounds.length} backgrounds`);
 }
 
 export { intelligentBackgroundInit };
